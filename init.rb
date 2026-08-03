a1d3eeddc4d38a11ad4e1860cc3e72f3676d377c
Sam Stouffer <samuellstouffer@gmail.com>
2026-08-03 13:29:29 -0400

    Bump version to 1.0.0

--- init.rb
+++ init.rb
@@ -40,7 +40,7 @@ Redmine::Plugin.register :nysenate_audit_utils do
   name 'NYSenate Audit Utils Plugin'
   author 'New York State Senate'
   description 'Audit utilities including ESS integration, reporting, user autofill, and packet creation for audit workflows'
-  version '0.11.0'
+  version '1.0.0'
   url 'https://github.com/nysenate/redmine-plugin-nysenate-audit-utils'
   author_url 'https://github.com/nysenate'
 
